b72d93bf31493282df858804e0db567853479cc6
Bhajanpreet Singh <bhajanpreet25@gmail.com>
2019-03-28 19:11:43 -0400

    Added faqs responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 (function(){
     var element = document.querySelector("span.year");
     element.textContent = new Date().getFullYear();
+
+
+
+    //Code for accordion
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
 })();
