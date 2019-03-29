e01ffd6e5f7311a3803fc9071b1dfc3205ff2093
Bhajanpreet Singh <bhajanpreet25@gmail.com>
2019-03-28 22:09:34 -0400

    Some changes and refactoring in script

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 (function(){
+
+    //Code for current year in the span.year
     var element = document.querySelector("span.year");
     element.textContent = new Date().getFullYear();
 
 
 
     //Code for accordion
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
+    var accordions = document.getElementsByClassName("accordion");
+    
+    for (let i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", function () {
             this.classList.toggle("active");
+            
             var panel = this.nextElementSibling;
+            
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
                 panel.style.maxHeight = panel.scrollHeight + "px";
             }
+
         });
     }
 
