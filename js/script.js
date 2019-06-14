631628b985cf8c921b72b7518a42711bb691e5cf
Bhajanpreet <codey-singh>
2019-06-14 16:03:58 -0400

    reformatted

--- js/script.js
+++ js/script.js
@@ -1,34 +1,27 @@
 /*
- * 
+ *
  * @author Bhajanpreet Singh
  * @description minimal js functionality to run accordions
  * and code to get year from current date
  * */
 
-(function () {
+(function() {
+  //Code for current year in the span.year
+  var element = document.querySelector("span.year");
+  element.textContent = new Date().getFullYear();
 
-    //Code for current year in the span.year
-    var element = document.querySelector("span.year");
-    element.textContent = new Date().getFullYear();
+  //Code for accordion
+  var accordions = document.getElementsByClassName("accordion");
 
-
-
-    //Code for accordion
-    var accordions = document.getElementsByClassName("accordion");
-
-    for (let i = 0; i < accordions.length; i++) {
-        accordions[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-
-            var panel = this.nextElementSibling;
-
-            if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-            } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-
-        });
-    }
-
+  for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 })();
